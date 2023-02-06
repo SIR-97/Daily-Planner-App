c8c45f0bd8f82beccd3292fe6ad1b52ab44d9fcc
SIR-97 <sirafiq.1997@gmail.com>
2023-02-06 13:43:21 +0000

    created and appended time and description classes respectively

--- starter/script.js
+++ starter/script.js
@@ -8,4 +8,21 @@ const businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 businessHours.forEach(hour => {
   // Create a timeblock element
   const timeblock = $("<div>").addClass("time-block row");
+
+// Created a time class 
+   const time = $("<div>")
+   .addClass("col-2 hour")
+   .text(`${hour}:00`);
+
+ // Create a description class
+
+ const description = $("<textarea>")
+   .addClass("col-8 description")
+   .attr("data-time", hour);
+
+ // Appended the time and description classes to the timeblock
+ timeblock.append(time, description);
+
+
+
 })
